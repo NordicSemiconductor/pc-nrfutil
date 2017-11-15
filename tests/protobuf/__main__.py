a3fb44b07c11a6fc25b96f6a584405946dbf488b
Iván Sánchez Ortega <ivan@sanchezortega.es>
2017-11-15 16:52:50 +0100

    linting after @chunfantasy's review

--- tests/protobuf/__main__.py
+++ tests/protobuf/__main__.py
@@ -10,29 +10,9 @@ sys.path.append(
     )
 )
 
-#from nordicsemi.version import NRFUTIL_VERSION
-#print NRFUTIL_VERSION
-
 from nordicsemi.dfu.init_packet_pb import InitPacketPB, HashTypes, DFUType, SigningTypes
 
-
 class TestStringMethods(unittest.TestCase):
-
-    #def test_upper(self):
-        #self.assertEqual('foo'.upper(), 'FOO')
-
-    #def test_isupper(self):
-        #self.assertTrue('FOO'.isupper())
-        #self.assertFalse('Foo'.isupper())
-
-    #def test_split(self):
-        #s = 'hello world'
-        #self.assertEqual(s.split(), ['hello', 'world'])
-        ## check that s.split fails when the separator is not a string
-        #with self.assertRaises(TypeError):
-            #s.split(2)
-
-
 
     def test_construct_from_empty_string(self):
         """
@@ -53,19 +33,19 @@ class TestStringMethods(unittest.TestCase):
         """
 
         i = InitPacketPB(
-                from_bytes = None,
+            from_bytes = None,
 
-                 hash_bytes = b"",
-                 hash_type = HashTypes.SHA256,
-                 dfu_type = DFUType.APPLICATION,
-                 is_debug=False,
-                 fw_version=0xffffffff,
-                 hw_version=0xffffffff,
-                 sd_size=0,
-                 app_size=1234,
-                 bl_size=0,
-                 sd_req=[0xffffffff]
-                )
+            hash_bytes = b"",
+            hash_type = HashTypes.SHA256,
+            dfu_type = DFUType.APPLICATION,
+            is_debug=False,
+            fw_version=0xffffffff,
+            hw_version=0xffffffff,
+            sd_size=0,
+            app_size=1234,
+            bl_size=0,
+            sd_req=[0xffffffff]
+            )
 
         protobuf_bytes = i.get_init_command_bytes(); # bytes only for the InitCommand
         hex_bytes = protobuf_bytes.encode('hex_codec');
@@ -83,26 +63,27 @@ class TestStringMethods(unittest.TestCase):
         """
 
         i = InitPacketPB(
-                from_bytes = None,
+            from_bytes = None,
 
-                 hash_bytes = b"",
-                 hash_type = HashTypes.SHA256,
-                 dfu_type = DFUType.APPLICATION,
-                 is_debug=False,
-                 fw_version=0xffffffff,
-                 hw_version=0xffffffff,
-                 sd_size=0,
-                 app_size=1234,
-                 bl_size=0,
-                 sd_req=[0xffffffff]
-                )
+            hash_bytes = b"",
+            hash_type = HashTypes.SHA256,
+            dfu_type = DFUType.APPLICATION,
+            is_debug=False,
+            fw_version=0xffffffff,
+            hw_version=0xffffffff,
+            sd_size=0,
+            app_size=1234,
+            bl_size=0,
+            sd_req=[0xffffffff]
+            )
 
         i.set_signature(b"signature bytes go here", SigningTypes.ECDSA_P256_SHA256)
 
         protobuf_bytes = i.get_init_packet_pb_bytes(); # Bytes for the whole Packet
         hex_bytes = protobuf_bytes.encode('hex_codec');
 
-        self.assertEqual(hex_bytes, "12450a280801122408ffffffff0f10ffffffff0f1a" "05ffffffff0f20002800300038d209420408031200480010001a177369676e61747572"
+        self.assertEqual(hex_bytes, "12450a280801122408ffffffff0f10ffffffff0f1a"
+        "05ffffffff0f20002800300038d209420408031200480010001a177369676e61747572"
         "6520627974657320676f2068657265")
 
 
@@ -113,19 +94,19 @@ class TestStringMethods(unittest.TestCase):
         """
 
         i = InitPacketPB(
-                from_bytes = None,
+            from_bytes = None,
 
-                 hash_bytes = b"",
-                 hash_type = HashTypes.SHA256,
-                 dfu_type = DFUType.APPLICATION,
-                 is_debug=False,
-                 fw_version=0xffffffff,
-                 hw_version=0xffffffff,
-                 sd_size=0,
-                 app_size=1234,
-                 bl_size=0,
-                 sd_req=[0xffffffff]
-                )
+            hash_bytes = b"",
+            hash_type = HashTypes.SHA256,
+            dfu_type = DFUType.APPLICATION,
+            is_debug=False,
+            fw_version=0xffffffff,
+            hw_version=0xffffffff,
+            sd_size=0,
+            app_size=1234,
+            bl_size=0,
+            sd_req=[0xffffffff]
+            )
 
         protobuf_bytes = i.get_init_packet_pb_bytes(); # Bytes for the whole Packet
         hex_bytes = protobuf_bytes.encode('hex_codec');
