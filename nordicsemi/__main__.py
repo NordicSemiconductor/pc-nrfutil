fd6b9bdba81127553e35824d64ad6d40b4fc3be6
Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>
2018-09-17 14:25:09 +0200

    NRFFOSDK-12346: Fixing boolean test of None

--- nordicsemi/__main__.py
+++ nordicsemi/__main__.py
@@ -537,7 +537,7 @@ def generate(zipfile,
     if hw_version == 'none':
         hw_version = None
 
-    if external_app == None:
+    if external_app is None:
         external_app = False
 
     # Convert multiple value into a single instance
