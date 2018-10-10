b12c22776d4de5c2985bca25ffa2f67140c2975f
anvi <andreas.vibeto@nordicsemi.no>
2018-10-10 09:26:57 +0200

    Added version 2 for BL DFU Settings page

--- nordicsemi/__main__.py
+++ nordicsemi/__main__.py
@@ -244,15 +244,49 @@ def settings():
               help='Custom start address for the settings page. If not specified, '
                    'then the last page of the flash is used.',
               type=BASED_INT_OR_NONE)
-
+@click.option('--no-backup',
+              help='Do not overwrite DFU settings backup page. If not specified, '
+                   'than the resulting .hex file will contain a copy of DFU settings, '
+                   'that will overwrite contents of DFU settings backup page.',
+              type=click.BOOL,
+              is_flag=True,
+              required=False)
+@click.option('--backup-address',
+              help='Address of the DFU settings backup page inside flash. '
+                   'By default, the backup page address is placed one page below DFU settings. '
+                   'The value is precalculated based on configured settings address '
+                   '(<DFU_settings_addrsss> - 0x1000).',
+              type=BASED_INT_OR_NONE)
+@click.option('--app-boot-validation',
+              help='The method of boot validation for application. Choose from:\n%s' % ('\n'.join(BOOT_VALIDATION_ARGS),),
+              required=False,
+              type=click.STRING)
+@click.option('--sd-boot-validation',
+              help='The method of boot validation for Softdevice. Choose from:\n%s' % ('\n'.join(BOOT_VALIDATION_ARGS),),
+              required=False,
+              type=click.STRING)
+@click.option('--softdevice',
+              help='The SoftDevice firmware file. Must be given if SD Boot Validation is used.',
+              required=False,
+              type=click.Path(exists=True, resolve_path=True, file_okay=True, dir_okay=False))
+@click.option('--key-file',
+              help='The private (signing) key in PEM fomat. Needed for ECDSA Boot Validation.',
+              required=False,
+              type=click.Path(exists=True, resolve_path=True, file_okay=True, dir_okay=False))
 def generate(hex_file,
-        family,
-        application,
-        application_version,
-        application_version_string,
-        bootloader_version,
-        bl_settings_version,
-        start_address):
+             family,
+             application,
+             application_version,
+             application_version_string,
+             bootloader_version,
+             bl_settings_version,
+             start_address,
+             no_backup,
+             backup_address,
+             app_boot_validation,
+             sd_boot_validation,
+             softdevice,
+             key_file):
 
     # Initial consistency checks
     if family is None:
@@ -283,8 +317,36 @@ def generate(hex_file,
         click.echo("Error: Bootloader DFU settings version required.")
         return
 
+    if sd_boot_validation and (sd_boot_validation not in BOOT_VALIDATION_ARGS):
+        click.echo("Error: --sd_boot_validation called with invalid argument. Must be one of:\n%s" % ("\n".join(BOOT_VALIDATION_ARGS)))
+        return
+
+    if app_boot_validation and (app_boot_validation not in BOOT_VALIDATION_ARGS):
+        click.echo("Error: --app_boot_validation called with invalid argument. Must be one of:\n%s" % ("\n".join(BOOT_VALIDATION_ARGS)))
+        return
+
+    if bl_settings_version == 1 and (app_boot_validation or sd_boot_validation):
+        click.echo("Error: Bootloader settings version 1 does not support boot validation.")
+        return
+
+    if (app_boot_validation is 'VALIDATE_ECDSA_P256_SHA256' and key_file is None) or \
+        (sd_boot_validation is 'VALIDATE_ECDSA_P256_SHA256' and key_file is None):
+        click.echo("Error: Key file must be given when 'VALIDATE_ECDSA_P256_SHA256' boot validation is used")
+        return
+
+    if app_boot_validation and not application:
+        click.echo("Error: --application hex file must be set when using --app_boot_validation")
+        return
+
+    if sd_boot_validation and not softdevice:
+        click.echo("Error: --softdevice hex file must be set when using --sd_boot_validation")
+        return
+
     sett = BLDFUSettings()
-    sett.generate(arch=family, app_file=application, app_ver=application_version_internal, bl_ver=bootloader_version, bl_sett_ver=bl_settings_version, custom_bl_sett_addr=start_address)
+    sett.generate(arch=family, app_file=application, app_ver=application_version_internal, bl_ver=bootloader_version,
+                  bl_sett_ver=bl_settings_version, custom_bl_sett_addr=start_address, no_backup=no_backup,
+                  backup_address=backup_address, app_boot_validation_type=app_boot_validation,
+                  sd_boot_validation_type=sd_boot_validation, sd_file=softdevice, key_file=key_file)
     sett.tohexfile(hex_file)
 
     click.echo("\nGenerated Bootloader DFU settings .hex file and stored it in: {}".format(hex_file))
