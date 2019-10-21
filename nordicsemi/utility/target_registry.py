c1dfd2a7875eec22ff2b0d8d84046197f25fd7d3
Sam Bristow <sam@bristow.nz>
2019-10-21 23:53:31 +1300

    Use raw string-literal for regex pattern
    Flake8 flagged  '\d' as an invalid escape sequence because the backslash
    hadn't been escaped before being passed through to the regex function.
    
    flake8 --select=W605
    
    Rather than messing around escaping everything we can just use raw
    string notation which doesn't  handle backslashed in any special way.
    
    https://docs.python.org/3/library/re.html

--- nordicsemi/utility/target_registry.py
+++ nordicsemi/utility/target_registry.py
@@ -71,7 +71,7 @@ class EnvTargetDatabase(TargetDatabase):
             self.targets = []
 
             for key, value in os.environ.items():
-                match = re.match("NORDICSEMI_TARGET_(?P<target>\d+)_(?P<key>[a-zA-Z_]+)", key)
+                match = re.match(r"NORDICSEMI_TARGET_(?P<target>\d+)_(?P<key>[a-zA-Z_]+)", key)
 
                 if match:
                     key_value = match.groupdict()
