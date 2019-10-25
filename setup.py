d16e2e27a9cdd81076c24ebacf7800f94697284c
Bjørn Inge Hanssen <bihanssen@users.noreply.github.com>
2019-10-25 12:30:49 +0200

    Update python version requirement specifier
    Since the dependency pc-ble-driver-py requires python 3.7.*, nrfutil is bound by the same requirement. Updating the python_requires in nrfutil to reflect this.

--- setup.py
+++ setup.py
@@ -148,7 +148,7 @@ setup(
                      '../libusb/x86/libusb-1.0.dll', '../libusb/x64/libusb-1.0.dll',
                      '../libusb/x64/libusb-1.0.dylib', '../libusb/LICENSE']
     },
-    python_requires='~= 3.7',
+    python_requires='~= 3.7.0',
     install_requires=reqs,
     zipfile=None,
     tests_require=[
