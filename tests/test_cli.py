8864a6092182d99058afc971331de2ccbdc077cd
Halvor <halvor-k@hotmail.com>
2020-03-27 14:58:14 +0100

    Fix/bdd (#311)
    * Add automatic flashing of connectivity firmware for non jlink devices
    
    * Fix test_cli
    
    * Update bdd
    
    * Add files to repo
    
    * Remove outdated tests
    
    * Fix help_information
    
    * Increase wait time

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -27,13 +27,13 @@ class TestManifest(unittest.TestCase):
 
         address = 'AABBCC112233'
         result = self.runner.invoke(self.cli, argumentList + [address])
-        self.assertTrue('Invalid address' not in result.output)
-        self.assertTrue('Failed to open' in str(result.exception))
+        self.assertTrue('Error: Invalid value for address' not in result.output)
+        self.assertTrue('Board not found' in str(result.exception))
 
         address = 'AA:BB:CC:11:22:33'
         result = self.runner.invoke(self.cli, argumentList + [address])
-        self.assertTrue('Invalid address' not in result.output)
-        self.assertTrue('Failed to open' in str(result.exception))
+        self.assertTrue('Error: Invalid value for address' not in result.output)
+        self.assertTrue('Board not found' in str(result.exception))
 
         address = 'AABBCC11223'
         result = self.runner.invoke(self.cli, argumentList + [address])
