41e724c02a56b3720d87d393f966df7909d9f9e0
Jørn Bøni Hofstad <jorn.hofstad@nordicsemi.no>
2022-09-28 08:00:45 +0200

    Revert PR 349

--- nordicsemi/dfu/package.py
+++ nordicsemi/dfu/package.py
@@ -176,8 +176,6 @@ class Package:
 
         self.firmwares_data = {}
 
-        if sd_req is not None:
-            init_packet_vars[PacketField.REQUIRED_SOFTDEVICES_ARRAY] = sd_req
 
         if app_fw:
             firmware_type = HexType.EXTERNAL_APPLICATION if is_external else HexType.APPLICATION
@@ -186,6 +184,9 @@ class Package:
                                      filename=app_fw,
                                      boot_validation_type=app_boot_validation_type,
                                      init_packet_data=init_packet_vars)
+
+        if sd_req is not None:
+            init_packet_vars[PacketField.REQUIRED_SOFTDEVICES_ARRAY] = sd_req
 
 
         if bootloader_fw:
